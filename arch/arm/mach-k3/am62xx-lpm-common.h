49ffca044af1c186175a9c28d4d2c52d734ad2ef
Markus Schneider-Pargmann (TI.com) <msp@baylibre.com>
2026-01-05 17:38:52 +0100

    arm: mach-k3: am62xx-lpm-common: Helper for LPM meta data address from DT
    Add a small helper that uses memory regions referenced by the R5
    devicetree node to calculate the LPM meta data address.
    
    Signed-off-by: Markus Schneider-Pargmann (TI.com) <msp@baylibre.com>

--- arch/arm/mach-k3/am62xx-lpm-common.h
+++ arch/arm/mach-k3/am62xx-lpm-common.h
@@ -13,6 +13,7 @@
 
 bool wkup_ctrl_is_lpm_exit(void);
 int wkup_ctrl_remove_can_io_isolation_if_set(void);
+int wkup_r5f_am62_lpm_meta_data_addr(u64 *meta_data_addr);
 void lpm_resume_from_ddr(u64 meta_data_addr);
 
 #endif  /* _AM62XX_LPM_COMMON_H_ */
