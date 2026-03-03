8147da6b61f15ff999def0a8d7dbd3cd046f9343
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-03-03 17:32:43 +0100

    ram: k3-ddrss: Add j722s DDR resume sequence
    As the j722s and am62p resume sequence differs on the way they are
    detecting that the board is resuming, we should have a specific
    compatible for each one of them.
    
    For that purpose, introduce ti,j722s-ddrss compatible.
    
    Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

--- drivers/ram/k3-ddrss/k3-ddrss.c
+++ drivers/ram/k3-ddrss/k3-ddrss.c
@@ -19,6 +19,7 @@
 #include <power-domain.h>
 #include <wait_bit.h>
 #include <power/regulator.h>
+#include <mach/hardware.h>
 #include <mach/k3-ddr.h>
 
 #include "k3-ddrss-lpm.h"
@@ -1301,11 +1302,18 @@ static const struct k3_ddrss_data j721s2_data = {
 	.flags = MULTI_DDR_SUBSYSTEM,
 };
 
+static const struct k3_ddrss_data j722s_data = {
+	.flags = SINGLE_DDR_SUBSYSTEM,
+	.is_lpm_resume = j7xx_board_is_resuming,
+	.ddrss_deassert_retention = am62xx_ddrss_deassert_retention,
+};
+
 static const struct udevice_id k3_ddrss_ids[] = {
 	{.compatible = "ti,am62a-ddrss", .data = (ulong)&am62xx_data, },
 	{.compatible = "ti,am64-ddrss", .data = (ulong)&k3_data, },
 	{.compatible = "ti,j721e-ddrss", .data = (ulong)&k3_data, },
 	{.compatible = "ti,j721s2-ddrss", .data = (ulong)&j721s2_data, },
+	{.compatible = "ti,j722s-ddrss", .data = (ulong)&j722s_data, },
 	{}
 };
 
