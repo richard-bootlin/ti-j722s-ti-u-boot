b97889886ae660f2ac7acaa46d19480d4fa56656
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-03-04 15:43:14 +0100

    new_flow: factorize common code
    Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

--- arch/arm/mach-k3/j722s/j722s_init.c
+++ arch/arm/mach-k3/j722s/j722s_init.c
@@ -8,13 +8,9 @@
 #include <spl.h>
 #include <asm/io.h>
 #include <asm/arch/hardware.h>
-#include <clk.h>
 #include <dm.h>
 #include <dm/uclass-internal.h>
 #include <dm/pinctrl.h>
-#include <elf.h>
-#include <linux/soc/ti/ti_sci_protocol.h>
-#include <power-domain.h>
 #include <remoteproc.h>
 
 #include "../sysfw-loader.h"
@@ -31,6 +27,8 @@ struct fwl_data cbass_main_fwls[] = {
  */
 u32 bootindex __section(".data");
 static struct rom_extended_boot_data bootdata __section(".data");
+
+struct lpm_addr_info mem_addr_lpm;
 
 #define CTRLMMR_MCU_RST_CTRL	(MCU_CTRL_MMR0_BASE + 0x18170)
 #define RST_CTRL_ESM_ERROR_RST_EN_Z_MASK  (~BIT(17))
@@ -180,117 +178,6 @@ static __maybe_unused void enable_mcu_esm_reset(void)
 
 	stat &= RST_CTRL_ESM_ERROR_RST_EN_Z_MASK;
 	writel(stat, CTRLMMR_MCU_RST_CTRL);
-}
-
-struct lpm_addr_info mem_addr_lpm;
-int extract_lpm_region(void)
-{
-	ofnode node;
-	fdt_addr_t lpm_reg_addr;
-	fdt_size_t lpm_reg_size;
-
-	node = ofnode_path("/reserved-memory/lpm-memories");
-	if (!ofnode_valid(node)) {
-		printf("lpm will not be functional \n");
-		return -ENODEV;
-	}
-
-	lpm_reg_addr = ofnode_get_addr(node);
-	if (lpm_reg_addr == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-	}
-
-	lpm_reg_size = ofnode_get_size(node);
-	if (lpm_reg_size == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-	}
-
-	mem_addr_lpm.context_save_addr = (u32 *)lpm_reg_addr;
-	mem_addr_lpm.atf_cert_addr =  (void *)(mem_addr_lpm.context_save_addr + LPM_IMAGE_SIZE);
-	mem_addr_lpm.optee_cert_addr = (void *)(mem_addr_lpm.atf_cert_addr + LPM_IMAGE_SIZE);
-	mem_addr_lpm.dm_save_addr = (void *)(mem_addr_lpm.optee_cert_addr + (2*LPM_IMAGE_SIZE));
-	mem_addr_lpm.size = lpm_reg_size;
-	return 0;
-}
-
-static unsigned long resume_to_dm_f(void)
-{
-	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
-	unsigned long loadaddr = 0, save_addr = 0;
-	int ret = 0;
-
-	loadaddr = (unsigned long)mem_addr_lpm.dm_save_addr;
-	if (!valid_elf_image(loadaddr))
-		panic("%s: DM-Firmware image is not valid, it cannot be loaded\n",
-		      __func__);
-	loadaddr = load_elf_image_phdr(loadaddr);
-	save_addr = (unsigned long)mem_addr_lpm.context_save_addr;
-	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, save_addr, mem_addr_lpm.size);
-	if (ret)
-		panic("TIFS lpm save addr fail : %x\n", ret);
-	/*
-	 * TIFS minimal context restore
-	 * This restores also the firewall
-	 */
-	ret = ti_sci->ops.lpm_ops.restore_context(ti_sci, 0);
-	if (ret)
-		panic("TIFS min_context_restore failed (%d)\n", ret);
-	/*
-	 * Restore TFA in msmc memory
-	 */
-	ret = ti_sci->ops.lpm_ops.decrypt_tfa(ti_sci,
-					      CONFIG_K3_ATF_LOAD_ADDR);
-	if (ret)
-		panic("%s: TIFS failed to decrytp TFA : %x\n", __func__, ret);
-
-	/* restore TFA resume vectore address in main core */
-	ret = ti_sci->ops.lpm_ops.core_resume(ti_sci);
-	if (ret)
-		panic("ATF failed to resume (%d)\n", ret);
-
-	return loadaddr;
-}
-
-
-static void resume_rproc_f(void)
-{
-	struct power_domain rproc_pwrdmn;
-	unsigned long gtc_rate;
-	struct udevice *dev;
-	struct clk gtc_clk;
-	void *gtc_base;
-	int ret;
-
-	ret = uclass_get_device_by_seq(UCLASS_REMOTEPROC, 1, &dev);
-	if (ret)
-		panic("Unknown remote processor 1 (%d)\n", ret);
-
-	ret = power_domain_get_by_index(dev, &rproc_pwrdmn, 1);
-	if (ret)
-		panic("power_domain_get_rproc() failed: %d\n", ret);
-
-	ret = clk_get_by_index(dev, 0, &gtc_clk);
-	if (ret)
-		panic("clk_get failed: %d\n", ret);
-
-	gtc_base = dev_read_addr_ptr(dev);
-	if (!gtc_base)
-		panic("Get GTC address failed\n");
-
-	gtc_rate = clk_get_rate(&gtc_clk);
-
-#define GTC_CNTCR_REG	0x0
-#define GTC_CNTFID0_REG	0x20
-#define GTC_CNTR_EN	0x3
-	/* TFA expect the Global Timebase Counter to be set-up */
-	writel((u32)gtc_rate, gtc_base + GTC_CNTFID0_REG);
-	writel(GTC_CNTR_EN, gtc_base + GTC_CNTCR_REG);
-
-	ret = power_domain_on(&rproc_pwrdmn);
-	if (ret)
-		panic("power_domain_on failed: %d\n", ret);
 }
 
 __weak int board_is_resuming(void)
