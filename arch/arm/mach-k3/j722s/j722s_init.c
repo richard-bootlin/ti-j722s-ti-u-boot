2f19a26dc3956e9833ffd00c6a3b1141f332a030
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-03-13 19:52:07 +0530

    PENDING: arm: mach-k3: j722s: Enable lpm resume flow for J722S
    Add support for resuming from suspend in board_init_f for J722S.
    The resume state of the SOC is identified and lpm resume
    sequence is followed accordingly.
    
    - Extract context address from devicetree and send to TIFS.
    - Power on the rproc cluster.
    - Replay the certificates attached to saved images of ATF and OPTEE.
    - Resume sequence for context restore and rproc resume.
    - Image entry to DM firmware.
    
    The context address area is firewalled by TIFS to protect it from
    other hosts. Same with the resume flow.
    
    Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

--- arch/arm/mach-k3/j722s/j722s_init.c
+++ arch/arm/mach-k3/j722s/j722s_init.c
@@ -11,6 +11,8 @@
 #include <dm.h>
 #include <dm/uclass-internal.h>
 #include <dm/pinctrl.h>
+#include <linux/soc/ti/ti_sci_protocol.h>
+#include <remoteproc.h>
 
 #include "../sysfw-loader.h"
 #include "../common.h"
@@ -156,6 +158,58 @@ static void k3_spl_init(void)
 		k3_dm_print_ver();
 }
 
+void lpm_process(void)
+{
+	int ret = 0;
+	unsigned long save_addr;
+	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
+
+	save_certificate();
+	save_addr = (unsigned long)mem_addr_lpm.context_save_addr;
+	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, save_addr,
+						mem_addr_lpm.size);
+	if (ret)
+		pr_err("TIFS lpm save addr fail\n");
+}
+
+int extract_lpm_region(void)
+{
+	ofnode node;
+	fdt_addr_t lpm_reg_addr;
+	fdt_size_t lpm_reg_size;
+
+	node = ofnode_path("/reserved-memory/lpm-memory");
+	if (!ofnode_valid(node)) {
+		printf("lpm will not be functional\n");
+		return -ENODEV;
+	}
+
+	lpm_reg_addr = ofnode_get_addr(node);
+	if (lpm_reg_addr == FDT_ADDR_T_NONE) {
+		printf("Can't find a valid reserved node!\n");
+		return -ENODEV;
+	}
+
+	lpm_reg_size = ofnode_get_size(node);
+	if (lpm_reg_size == FDT_ADDR_T_NONE) {
+		printf("Can't find a valid reserved node!\n");
+		return -ENODEV;
+	}
+
+	mem_addr_lpm.context_save_addr = (u32 *)lpm_reg_addr;
+	mem_addr_lpm.atf_cert_addr = mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE;
+	mem_addr_lpm.optee_cert_addr = mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE;
+	mem_addr_lpm.dm_save_addr = mem_addr_lpm.optee_cert_addr + (2 * FW_IMAGE_SIZE);
+	mem_addr_lpm.size = lpm_reg_size;
+
+	return 0;
+}
+
+__weak int board_is_resuming(void)
+{
+	return 0;
+}
+
 static void k3_mem_init(void)
 {
 	struct udevice *dev;
@@ -189,6 +243,35 @@ void board_init_f(ulong dummy)
 
 	k3_spl_init();
 	k3_mem_init();
+	if (board_is_resuming()) {
+		typedef void __noreturn (*image_entry_noargs_t)(void);
+		image_entry_noargs_t image_entry;
+		unsigned long loadaddr;
+		size_t size_int;
+		void *image_addr;
+		int ret;
+
+		ret = extract_lpm_region();
+		if (ret)
+			panic("Cannot find valid LPM address range..LPM resume failed\n");
+
+		image_addr = (void *)mem_addr_lpm.atf_cert_addr;
+		ret = rproc_load(1, (ulong)image_addr, 0x200);
+		if (ret)
+			panic("rproc failed to be initialized (%d)\n", ret);
+
+		image_addr = mem_addr_lpm.atf_cert_addr;
+		size_int = FW_IMAGE_SIZE;
+		ti_secure_image_replay_cert(&image_addr, &size_int);
+		image_addr = mem_addr_lpm.optee_cert_addr;
+		ti_secure_image_replay_cert(&image_addr, &size_int);
+		loadaddr = resume_to_dm_f();
+		printf("Starting ATF on ARM64 core...\n\n");
+		resume_rproc_f();
+		image_entry = (image_entry_noargs_t)loadaddr;
+		image_entry();
+	}
+
 	setup_qos();
 
 	if (IS_ENABLED(CONFIG_ESM_K3)) {
