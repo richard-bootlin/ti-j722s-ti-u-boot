3bbf6ea58cec0022b630ff58b62ce197c007cce7
Abhash Kumar Jha <a-kumar2@ti.com>
2026-03-13 19:28:53 +0530

    board: evm: Enable de-isolation of IOs at resume for j7200 and j784s4
    When resuming from IO_ONLY_PLUS_DDR low power mode, the IOs are
    isolated. Remove that isolation in resume sequence.
    
    On j7200, The canuart IOs are isolated whereas on j784s4, the
    mcu_general IOs are isolated.
    
    Signed-off-by: Abhash Kumar Jha <a-kumar2@ti.com>

--- board/ti/j721e/evm.c
+++ board/ti/j721e/evm.c
@@ -18,6 +18,7 @@
 #include <dm.h>
 #include <asm/arch/k3-ddr.h>
 #include <power/pmic.h>
+#include <wait_bit.h>
 
 #include "../common/board_detect.h"
 #include "../common/fdt_ops.h"
@@ -473,13 +474,41 @@ err_free_gpio:
 
 #if (IS_ENABLED(CONFIG_SPL_BUILD) && IS_ENABLED(CONFIG_TARGET_J7200_R5_EVM))
 
+static void clear_isolation(void)
+{
+	int ret;
+	const void *wait_reg = (const void *)(WKUP_CTRL_MMR0_BASE + CANUART_WAKE_STAT1);
+
+	/* un-set the magic word for canuart IOs */
+	writel(IO_ISO_MAGIC_VAL, WKUP_CTRL_MMR0_BASE + CANUART_WAKE_CTRL);
+	writel((IO_ISO_MAGIC_VAL + 0x1), WKUP_CTRL_MMR0_BASE + CANUART_WAKE_CTRL);
+	writel(IO_ISO_MAGIC_VAL, WKUP_CTRL_MMR0_BASE + CANUART_WAKE_CTRL);
+
+	/* wait for CANUART_IO_MODE bit to be cleared */
+	ret = wait_for_bit_32(wait_reg,
+			      CANUART_WAKE_STAT1_CANUART_IO_MODE,
+			      false,
+			      DEISOLATION_TIMEOUT_MS,
+			      false);
+	if (ret < 0)
+		pr_err("Deisolation timeout");
+}
+
 bool j7xx_board_is_resuming(void)
 {
 	struct udevice *pmica;
+	u32 pmctrl_val = readl(PMCTRL_IO_1);
 	int ret;
 
 	if (gd_k3_resuming() != K3_RESUME_STATE_UNKNOWN)
 		goto end;
+
+	if ((pmctrl_val & IO_ISO_STATUS) == IO_ISO_STATUS) {
+		clear_isolation();
+		gd_set_k3_resuming(K3_RESUME_STATE_RESUMING);
+		debug("Resuming from IO_DDR mode\n");
+		goto end;
+	}
 
 	ret = uclass_get_device_by_name(UCLASS_PMIC,
 					"pmic@48", &pmica);
