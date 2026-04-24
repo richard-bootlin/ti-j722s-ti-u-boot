32c36dfacb3e2b44c1755547a60f4aecd36f58f5
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-04-24 17:02:42 +0200

    add j784s4/j721s2 ddr resume

--- drivers/ram/k3-ddrss/k3-ddrss.c
+++ drivers/ram/k3-ddrss/k3-ddrss.c
@@ -122,6 +122,7 @@ enum intrlv_size {
 
 struct k3_ddrss_data {
 	u32 flags;
+	bool lpm_init_only;
 	bool (*is_lpm_resume)(void);
 	void (*ddrss_deassert_retention)(void);
 };
@@ -1157,6 +1158,9 @@ static int k3_ddrss_probe(struct udevice *dev)
 	k3_lpddr4_init(ddrss);
 	k3_lpddr4_hardware_reg_init(ddrss);
 
+	if (is_lpm_resume && ddrss_data->lpm_init_only)
+		return k3_ddrss_init_freq(ddrss);
+
 	if (is_lpm_resume)
 		k3_ddrss_self_refresh_exit(ddrss->ddrss_ctl_cfg);
 
@@ -1299,6 +1303,8 @@ static const struct k3_ddrss_data am62xx_data = {
 
 static const struct k3_ddrss_data j721s2_data = {
 	.flags = MULTI_DDR_SUBSYSTEM,
+	.lpm_init_only = true,
+	.is_lpm_resume = j722s_wkup_conf_boot_is_resume,
 };
 
 static const struct k3_ddrss_data j722s_data = {
